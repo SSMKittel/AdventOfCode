e14ca338752f9eda05189454d154a8bc9ebe9fb9
Samuel Kittel <S.S.M.Kittel+github@gmail.com>
2019-12-04 20:58:50 +1030

    Add day 1 second half

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -1,11 +1,28 @@
 fn main() {
     let input_mass = include_str!("input.txt");
-    let total_fuel = input_mass.split_whitespace()
+    let module_masses = input_mass.split_whitespace()
         .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let simple_fuel_mass = module_masses.iter()
+        .map(calc_fuel_simple)
+        .sum::<u32>();
+
+    let real_fuel_mass = module_masses.iter()
         .map(calc_fuel)
         .sum::<u32>();
-    println!("Total Fuel Required: {}", total_fuel);
+
+    println!("Total Fuel Required (Simple): {}", simple_fuel_mass);
+    println!("Total Fuel Required (Including Fuel Mass): {}", real_fuel_mass);
 }
-fn calc_fuel(mass:u32) -> u32 {
+fn calc_fuel_simple(mass:&u32) -> u32 {
   mass / 3 - 2
+}
+fn calc_fuel(mass:&u32) -> u32 {
+    let tmp = mass / 3;
+    if tmp <= 2 {
+        return 0;
+    }
+    let tmp = tmp - 2;
+    tmp + calc_fuel(&tmp)
 }
