558a8dc556fb87646ec8339123bfe32a402493be
Samuel Kittel <S.S.M.Kittel+github@gmail.com>
2019-12-09 00:15:17 +1030

    day 6 part 2

--- 2019/day06/src/main.rs
+++ 2019/day06/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::ops::Index;
 use std::borrow::Borrow;
 
@@ -11,6 +12,7 @@ fn main() {
     let system = build_system(&orbits);
 
     println!("Direct and Indirect: {}", system.count_all_direct_indirect());
+    println!("YOU -> SAN: {:?}", system.transfers("YOU", "SAN"));
 }
 
 fn build_system<'a>(orbits: &[Orbit<'a>]) -> SolarSystem<'a> {
@@ -79,10 +81,39 @@ impl<'a> SolarSystem<'a> {
     }
     fn count_direct_indirect(&self, soi: &SphereOfInfluence<'a>, depth: usize) -> usize {
         let mut total: usize = depth;
-        for root in &soi.children {
-            total += self.count_direct_indirect(&self[*root], depth + 1)
+        for child in &soi.children {
+            total += self.count_direct_indirect(&self[*child], depth + 1)
         }
         total
+    }
+    fn path<'b: 'a>(&self, start: &SphereOfInfluence<'a>, target: &'b str) -> Vec<&'a str> {
+        if start.name == target {
+            return vec![start.name]
+        }
+        for child in &start.children {
+            let mut p = self.path(&self[*child], target);
+            if !p.is_empty() {
+                p.insert(0, start.name);
+                return p;
+            }
+        }
+        vec![]
+    }
+    fn transfers(&self, target1: &str, target2: &str) -> Option<usize> {
+        for root in &self.roots {
+            let p1 = self.path(&self[*root], target1);
+            let p2 = self.path(&self[*root], target2);
+            if !p1.is_empty() && !p2.is_empty() {
+                let p1hs = p1.iter().collect::<HashSet<_>>();
+                let p2hs = p2.iter().collect::<HashSet<_>>();
+
+                // Counting the hops by counting the number of entities would normally be one more than the number of jumps between them
+                // however the common ancestor node is being removed by the difference as well and corrects the value
+                // -2 to exclude target1 and target2.
+                return Some(p1hs.symmetric_difference(&p2hs).count() - 2)
+            }
+        }
+        None
     }
 }
 impl<'a> Index<SoiId> for SolarSystem<'a> {
@@ -112,7 +143,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_program() {
+    fn test_transfers() {
+        let orbits = include_str!("input-test-you-san.txt")
+            .lines()
+            .map(build_orbit)
+            .collect::<Vec<_>>();
+
+        let system = build_system(&orbits);
+        assert_eq!(system.transfers("YOU", "SAN"), Some(4));
+    }
+
+    #[test]
+    fn test_pathing() {
+        let orbits = include_str!("input-test-you-san.txt")
+            .lines()
+            .map(build_orbit)
+            .collect::<Vec<_>>();
+
+        let system = build_system(&orbits);
+        assert_eq!(system.path(&system["COM"], "YOU"), vec!["COM", "B", "C", "D", "E", "J", "K", "YOU"]);
+        assert_eq!(system.path(&system["COM"], "SAN"), vec!["COM", "B", "C", "D", "I", "SAN"]);
+    }
+
+    #[test]
+    fn test_direct_indirect_count() {
         let orbits = include_str!("input-test.txt")
             .lines()
             .map(build_orbit)
