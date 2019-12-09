622f31eeb316067f4bfe22648949e8757ee64d5b
Samuel Kittel <S.S.M.Kittel+github@gmail.com>
2019-12-10 06:55:38 +1030

    day 8 part 2

--- 2019/day08/src/main.rs
+++ 2019/day08/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
     let img = include_str!("input.txt").chars().collect::<Vec<_>>();
-    let mut layer_id = 1;
 
     let checksum = img.chunks(25 * 6)
         .map(|layer|
@@ -10,7 +9,21 @@ fn main() {
                 * layer.iter().filter(|&&pixel| pixel == '2').count()
             )
         )
-        .min_by_key(|(c0, cs)| *c0)
-        .map(|(c0, cs)| cs);
-    println!("{:?}", checksum);
+        .min_by_key(|(c0, _)| *c0)
+        .map(|(_, cs)| cs);
+    println!("Checksum {:?}", checksum.unwrap());
+
+    let mut img_rendered = ['2'; 25 * 6];
+    for layer in img.chunks(25 * 6) {
+        for i in 0..(25 * 6) {
+            if img_rendered[i] == '2' {
+                img_rendered[i] = layer[i];
+            }
+        }
+    }
+    println!("Image:");
+    for r in img_rendered.chunks(25) {
+        // Colours inverted because it's easier to read
+        println!("{}", r.iter().map(|c| if *c == '1' {'█'} else {' '}).collect::<String>());
+    }
 }
